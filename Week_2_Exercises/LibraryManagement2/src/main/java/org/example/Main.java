2849296892f0aa82a231756eb4e2998eef07ac88
SARAN1231 <saranprakash3112@gmail.com>
2024-08-01 16:58:49 +0530

    completed next 3 exe in week2 (spring)

---
+++ Week_2_Exercises/LibraryManagement2/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+// src/main/java/com/library/Main.java
+package org.example;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        bookService.manageBooks();
+    }
+}
